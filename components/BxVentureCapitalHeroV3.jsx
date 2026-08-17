d3490f96c992cec20ffc98e69e001293d1041893
webservices-at-collateral <webservices@collateral.com>
2026-08-17 19:14:19 -0300

    Fontsize Update

--- components/BxVentureCapitalHeroV3.jsx
+++ components/BxVentureCapitalHeroV3.jsx
@@ -95,7 +95,7 @@ export default function BxVentureCapitalHeroV3() {
           className="max-w-[524px] text-left"
           style={{
             fontFamily: "var(--font-produkt), serif",
-            fontSize: "16px",
+            fontSize: "20px",
             fontWeight: 400,
             lineHeight: "1.4",
             color: "rgba(255,255,255,0.72)",
